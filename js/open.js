d686348135eb9feff30d5cc97bbc412fe528bccc
miyaajd <ehdal1032@gmail.com>
2025-09-01 11:51:41 +0900

    popUp open/Close

---
+++ js/open.js
@@ -0,0 +1,9 @@
+$(function (){
+    // console.log("check");
+    $("#openBtn").click(function(){
+        $("#popup").show(200)
+    })
+    $("#closeBtn").click(function(){
+        $("#popup").hide(200)
+    })
+})
