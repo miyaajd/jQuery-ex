a09836290c4d1c09c1f5a6f026672a2d73dad391
miyaajd <ehdal1032@gmail.com>
2025-09-01 12:17:29 +0900

    toggleClass

--- js/toggle.js
+++ js/toggle.js
@@ -1,6 +1,8 @@
 $(function(){
     $("#btn").click(function(){
         // $(".box").toggle(200)
-        $(".box").slideToggle(300)
+        // $(".box").slideToggle(300)
+        // $(".box").fadeToggle(300)
+        $(".box").toggleClass("active")
     })
 })
