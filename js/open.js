731d7046e5442704c39b873ed4ecf595812e1099
miyaajd <ehdal1032@gmail.com>
2025-09-01 12:03:52 +0900

    toggle

--- js/open.js
+++ js/open.js
@@ -1,9 +1,18 @@
 $(function (){
     // console.log("check");
+    // $("#openBtn").click(function(){
+    //     $("#popup").show(200)
+    // })
+    // $("#closeBtn").click(function(){
+    //     $("#popup").hide(200)
+    // })
+    // $("#openBtn").click(function(){
+    //     $("#popup").fadeIn(300)
+    // })
     $("#openBtn").click(function(){
-        $("#popup").show(200)
+        $("#popup").toggle(200)
     })
     $("#closeBtn").click(function(){
-        $("#popup").hide(200)
+        $("#popup").fadeOut(300)
     })
 })
