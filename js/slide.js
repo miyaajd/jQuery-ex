5c142104c239d69e39e7e42a0762298e5fda8129
miyaajd <ehdal1032@gmail.com>
2025-09-03 13:07:49 +0900

    slide fade in / out

--- js/slide.js
+++ js/slide.js
@@ -1,4 +1,5 @@
 $(function () {
+  // horizontal slide
   const slideList = $(".slide-list");
   const slideWidth = $(".slide-list li").width();
   console.log(slideWidth);
@@ -8,6 +9,7 @@ $(function () {
       $(this).css({ marginLeft: 0 });
     });
   };
+  //   vertical slide
   setInterval(moveSlide, 4000);
   const verSlideList = $(".slide-list-ver");
   const slideHeight = $(".slide-list-ver li").height();
@@ -18,4 +20,17 @@ $(function () {
     });
   };
   setInterval(verMoveSlide, 4000);
+  //   fade in / out
+  const fadeSlide = $(".slide-fade li");
+  let firstIndex = 0; // const 로는 재할당이 안되기 때문에 let 을 써야함
+  const moveFade = () => {
+    let next = (firstIndex + 1) % fadeSlide.length;
+    // console.log(next);
+    fadeSlide.eq(firstIndex).fadeOut(1000);
+    // console.log(fadeSlide.eq(firstIndex));
+    fadeSlide.eq(next).fadeIn(1000);
+    firstIndex = next; //재할당 해야해서 let 사용
+  };
+  fadeSlide.hide().eq(0).show()
+  setInterval(moveFade, 4000);
 });
