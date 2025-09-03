1f36ae3984138bd1ac920a42e95e979673a75518
miyaajd <ehdal1032@gmail.com>
2025-09-03 12:33:12 +0900

    vertical slide

--- js/slide.js
+++ js/slide.js
@@ -4,9 +4,18 @@ $(function () {
   console.log(slideWidth);
   const moveSlide = () => {
     slideList.animate({ marginLeft: -slideWidth }, 1000, function () {
-      $(this).append($(this).find("li:first"))
-      $(this).css({marginLeft : 0})
+      $(this).append($(this).find("li:first"));
+      $(this).css({ marginLeft: 0 });
     });
   };
-  setInterval(moveSlide, 3000);
+  setInterval(moveSlide, 4000);
+  const verSlideList = $(".slide-list-ver");
+  const slideHeight = $(".slide-list-ver li").height();
+  const verMoveSlide = () => {
+    verSlideList.animate({ marginTop: -slideHeight }, 1000, function () {
+      $(this).append($(this).find("li:first"));
+      $(this).css({ marginTop: 0 });
+    });
+  };
+  setInterval(verMoveSlide, 4000);
 });
