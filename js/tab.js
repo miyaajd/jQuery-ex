0e4131f121623630f49f2635ae11bd6649284393
miyaajd <ehdal1032@gmail.com>
2025-09-04 12:41:30 +0900

    madal done

--- js/tab.js
+++ js/tab.js
@@ -3,4 +3,11 @@ $(function () {
     e.preventDefault();
     $(this).parent().addClass("active").siblings().removeClass("active");
   });
+//   modal
+$(".notice-list li:first-child").click(function(){
+    $("#modal").addClass("active")
+})
+$(".modal-btn").click(function(){
+    $("#modal").removeClass("active")
+})
 });
